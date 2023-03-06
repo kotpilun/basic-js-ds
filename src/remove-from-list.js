fe8ae97d8696949ffec7ebd743346ef780e7f470
kotpilun <kotpilun3m@gmail.com>
2023-03-06 07:13:38 +0300

    remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,35 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
+
+// function convertArrayToList(arr) {
+//   return arr.reverse().reduce((acc, cur) => {
+//     if (acc) {
+//       const node = new ListNode(cur);
+//       node.next = acc;
+//       return node;
+//     }
+
+//     return new ListNode(cur);
+//   }, null);
+// }
+
+// function find(list, data) {
+//   if (list === null) {
+//     return list;
+//   } else if (list.value === data) {
+//     return true;
+//   } else {
+//     return find(list.next, data);
+//   }
+// }
+
+// const arr = convertArrayToList([3, 1, 2, 3, 4, 5]);
+
+// console.log(
+//   find(arr, 10)
+// );
+
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +51,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+  let currNode = l;
+  if (currNode != null) {
+    while(currNode.next) {
+      if (currNode.next.value === k) {
+        currNode.next = currNode.next.next;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+  }
+  return l;
 }
+
+// const arr = convertArrayToList([3, 1, 2, 3, 4, 5, 3]);
+
+// console.log(
+//   removeKFromList(arr, 3)
+// );
 
 module.exports = {
   removeKFromList
 };
+
+
